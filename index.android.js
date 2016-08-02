36ed248b55819987a3407250f2dc39cac77ca829
田想兵 <tianxb@shinemo.com>
2016-08-02 09:52:36 +0800

    init

---
+++ index.android.js
@@ -0,0 +1,54 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, {
+  Component
+} from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+class MyProject extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Hello,world.
+        </Text>
+        <Text style={styles.instructions}>
+          To 掌心化屎
+        </Text>
+        <Text style={styles.instructions}>
+          叶雨冰星 QQ群：5678537
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('MyProject', () => MyProject);
